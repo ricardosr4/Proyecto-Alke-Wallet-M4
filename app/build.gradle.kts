cd3fd36c8ef72493a461e918cb376b440e701bcc
Ricardo Soto <ricardosr4@gmail.com>
2026-07-17 16:15:47 -0400

    create login sing up ui ok

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 android {
     namespace = "com.example.proyectoalkewallet"
     compileSdk {
-        version = release(36) {
+        version = release(37) {
             minorApiLevel = 1
         }
     }
@@ -13,7 +13,7 @@ android {
     defaultConfig {
         applicationId = "com.example.proyectoalkewallet"
         minSdk = 30
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
---
+++ app/src/main/java/com/example/proyectoalkewallet/LoginSingup.kt
@@ -0,0 +1,24 @@
+package com.example.proyectoalkewallet
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LoginSingup : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login_singup)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
+/* en este layout estaria pendiente decidir si dejamos el boton como componente card , que es como esta ahora o si lo cambiamos directamente por un boton
+tambien estaria pendiente agregar los string como resource a la clase de string resource
+
+ */
