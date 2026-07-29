907e4aea2c5240c1f61498f34cdc78868b23c404
Ricardo Soto <ricardosr4@gmail.com>
2026-07-28 22:13:33 -0400

    create sing up ui ok, and create home ui, and home empty, app compile ok

--- app/src/main/java/com/example/proyectoalkewallet/Login.kt
+++ app/src/main/java/com/example/proyectoalkewallet/Login.kt
@@ -1,6 +1,8 @@
 package com.example.proyectoalkewallet
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,12 @@ class Login : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val actionLogin = findViewById<View>(R.id.btnLogin)
+
+        actionLogin.setOnClickListener {
+            startActivity(Intent(this, Home::class.java))
         }
     }
+
 }
--- app/src/main/java/com/example/proyectoalkewallet/LoginSingup.kt
+++ app/src/main/java/com/example/proyectoalkewallet/LoginSingup.kt
@@ -1,24 +1,26 @@
 package com.example.proyectoalkewallet
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class LoginSingup : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login_singup)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btnActionSignup = findViewById<View>(R.id.btn_action_signup)
+        val tvActionLogin = findViewById<View>(R.id.tv_action_login)
+
+        btnActionSignup.setOnClickListener {
+            startActivity(Intent(this, Singup::class.java))
+        }
+
+        tvActionLogin.setOnClickListener {
+            startActivity(Intent(this, Login::class.java))
         }
     }
 }
-/* en este layout estaria pendiente decidir si dejamos el boton como componente card , que es como esta ahora o si lo cambiamos directamente por un boton
-tambien estaria pendiente agregar los string como resource a la clase de string resource
-
- */
